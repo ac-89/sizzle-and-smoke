403000a96b04f9ad52d8b501e7bab578917d1886
ac-89 <alancarroll89@gmail.com>
2023-06-09 09:42:33 +0100

    Add media queries. Create collapsible nav menu.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+document.querySelector(".nav-box").addEventListener("click", function () {
+  const menu = document.getElementById("nav_ul");
+  const computedStyle = window.getComputedStyle(menu);
+  console.log("click", computedStyle.display);
+  if (computedStyle.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
